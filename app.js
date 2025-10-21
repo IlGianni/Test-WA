7cf949edcd9a9aaa44d87739325648ab8c2df41e
_Gianni_ <marco.macherelli@spacedesign-italia.it>
2025-10-21 17:10:15 +0200

    Fix string quotes and improve formatting in app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Import Express.js
-const express = require("express");
+const express = require('express');
 
 // Create an Express app
 const app = express();
@@ -12,16 +12,11 @@ const port = process.env.PORT || 3000;
 const verifyToken = process.env.VERIFY_TOKEN;
 
 // Route for GET requests
-app.get("/", (req, res) => {
-  console.log(req);
-  const {
-    "hub.mode": mode,
-    "hub.challenge": challenge,
-    "hub.verify_token": token,
-  } = req.query;
+app.get('/', (req, res) => {
+  const { 'hub.mode': mode, 'hub.challenge': challenge, 'hub.verify_token': token } = req.query;
 
-  if (mode === "subscribe" && token === verifyToken) {
-    console.log("WEBHOOK VERIFIED");
+  if (mode === 'subscribe' && token === verifyToken) {
+    console.log('WEBHOOK VERIFIED');
     res.status(200).send(challenge);
   } else {
     res.status(403).end();
@@ -29,8 +24,8 @@ app.get("/", (req, res) => {
 });
 
 // Route for POST requests
-app.post("/", (req, res) => {
-  const timestamp = new Date().toISOString().replace("T", " ").slice(0, 19);
+app.post('/', (req, res) => {
+  const timestamp = new Date().toISOString().replace('T', ' ').slice(0, 19);
   console.log(`\n\nWebhook received ${timestamp}\n`);
   console.log(JSON.stringify(req.body, null, 2));
   res.status(200).end();
